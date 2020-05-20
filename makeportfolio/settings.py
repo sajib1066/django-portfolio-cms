d132419b8cbce70cff829c8bb993cd22f157700b
sajib1066 <sajib1066@gmail.com>
2020-05-20 12:20:21 +0600

    setup template

--- makeportfolio/settings.py
+++ makeportfolio/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'service',
     'skill',
     'education',
-    'experience'
+    'experience',
     'work',
 ]
 
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'makeportfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
