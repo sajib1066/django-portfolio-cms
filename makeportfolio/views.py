98ab99f7bccc62b80b0b47309eb3d56b18a2c22c
sajib1066 <sajib1066@gmail.com>
2020-05-22 16:49:05 +0600

    setup home page

--- makeportfolio/views.py
+++ makeportfolio/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def index_page(request):
-    return render(request, 'index.html')
+    return render(request, 'home/index.html')
 
 def dashboard(request):
     return render(request, 'dashboard/index.html')
