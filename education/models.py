0a4db75847fb931f23e149d89f4fab170f499a4d
sajib1066 <sajib1066@gmail.com>
2020-05-24 14:31:22 +0600

    create education field

--- education/models.py
+++ education/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from account.models import User
+from makeportfolio.helper import get_current_user
+
+class Education(models.Model):
+    current_user = get_current_user
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=current_user)
+    degree = models.CharField(max_length=220, unique=True)
+    board = models.CharField(max_length=120)
+    institute = models.CharField(max_length=220)
+    passing_year = models.CharField(max_length=4)
+    result = models.DecimalField(max_digits=4, decimal_places=2)
+
+    def __str__(self):
+        return self.degree
