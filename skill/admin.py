107e9200bed22a5d4fd3b875d446bda8f32b2975
sajib1066 <sajib1066@gmail.com>
2020-05-24 14:21:44 +0600

    create skill model

--- skill/admin.py
+++ skill/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import MySkill, Skill
+
+admin.site.register(MySkill)
+admin.site.register(Skill)
--- skill/models.py
+++ skill/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from account.models import User
 
-# Create your models here.
+from makeportfolio.helper import get_current_user
+
+class MySkill(models.Model):
+    current_user = get_current_user
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=current_user)
+    name = models.CharField(max_length=20)
+    persent = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Skill(models.Model):
+    current_user = get_current_user
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=current_user)
+    about = models.TextField()
+    skills = models.ManyToManyField(MySkill)
+
+    def __str__(self):
+        return self.about[10]
