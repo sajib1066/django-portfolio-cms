90166e2cc48afa4a47e67a028d54cd2d397b1d3f
sajib1066 <sajib1066@gmail.com>
2020-05-27 11:42:44 +0600

    set login required

--- makeportfolio/views.py
+++ makeportfolio/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 def index_page(request):
     return render(request, 'home/index.html')
-
+@login_required(login_url='authentication')
 def dashboard(request):
     return render(request, 'dashboard/index.html')
