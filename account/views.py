98a9e08b7710beafd5d982b6f0bc1736553da08d
sajib1066 <sajib1066@gmail.com>
2020-05-27 11:05:33 +0600

    create login

--- account/views.py
+++ account/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
 from .forms import LoginForm, RegistrationForm
 
 def authentication(request):
     loginforms = LoginForm()
     registrationforms = RegistrationForm()
+    if request.method == 'POST':
+        loginforms = LoginForm(request.POST)
+        if loginforms.is_valid():
+            email = loginforms.cleaned_data['email']
+            password = loginforms.cleaned_data['password']
+            user = authenticate(email=email, password=password)
+            if user:
+                login(request, user)
+                return redirect('dashboard')
     context = {
         'login': loginforms,
         'registration': registrationforms
