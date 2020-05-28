b9569e7a8dbb80731181434b4ab18a897c868ebb
sajib1066 <sajib1066@gmail.com>
2020-05-28 11:44:26 +0600

    set profile photo is dashboard

--- makeportfolio/urls.py
+++ makeportfolio/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
 
 from .views import index_page, dashboard
+from . import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +27,6 @@ urlpatterns = [
     path('account/', include('account.urls')),
     path('dashboard/', dashboard, name='dashboard'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
