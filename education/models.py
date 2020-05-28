bdde0a09e648bccf2436b186a72b842126d1f4b1
sajib1066 <sajib1066@gmail.com>
2020-05-28 20:01:53 +0600

    set unique together

--- education/models.py
+++ education/models.py
@@ -6,11 +6,14 @@ from makeportfolio.helper import get_current_user
 class Education(models.Model):
     current_user = get_current_user
     user = models.OneToOneField(User, on_delete=models.CASCADE, default=current_user)
-    degree = models.CharField(max_length=220, unique=True)
+    degree = models.CharField(max_length=220)
     board = models.CharField(max_length=120)
     institute = models.CharField(max_length=220)
     passing_year = models.CharField(max_length=4)
     result = models.DecimalField(max_digits=4, decimal_places=2)
+
+    class Meta:
+        unique_together = ['user', 'degree']
 
     def __str__(self):
         return self.degree
