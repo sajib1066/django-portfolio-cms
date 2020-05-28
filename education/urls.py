fc91bae4c6deab8fa715caf26b59868837d6d39c
sajib1066 <sajib1066@gmail.com>
2020-05-28 19:52:02 +0600

    create education list

--- education/urls.py
+++ education/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import education
+from .views import education, education_list
 
 urlpatterns = [
     path('', education, name='education'),
+    path('list/', education_list, name='education-list'),
 ]
--- education/views.py
+++ education/views.py
@@ -12,3 +12,11 @@ def education(request):
         'forms': forms
     }
     return render(request, 'dashboard/education.html', context)
+
+def education_list(request):
+    education = Education.objects.filter(user=request.user)
+    print(education)
+    context = {
+        'education': education
+    }
+    return render(request, 'dashboard/education-list.html', context)
