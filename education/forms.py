1ff09e9a52768d963605893a351544b5dc2a2c11
sajib1066 <sajib1066@gmail.com>
2020-05-29 09:03:38 +0600

    update add_education form

--- education/forms.py
+++ education/forms.py
@@ -6,3 +6,11 @@ class EducationForm(forms.ModelForm):
     class Meta:
         model = Education
         fields = ['degree', 'board', 'institute', 'passing_year', 'result']
+
+        widgets = {
+            'degree': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your degree title'}),
+            'board': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your board name'}),
+            'institute': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your institute name'}),
+            'passing_year': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your passing year'}),
+            'result': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your result'}),
+        }
