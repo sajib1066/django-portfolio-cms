452bde4c41e79432048f719c33098e6fde8c00fe
sajib1066 <sajib1066@gmail.com>
2020-05-29 18:32:26 +0600

    update experience model

--- experience/models.py
+++ experience/models.py
@@ -6,7 +6,7 @@ from makeportfolio.helper import get_current_user
 
 class Experience(models.Model):
     current_user = get_current_user
-    user = models.OneToOneField(User, on_delete=models.CASCADE, default=current_user)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=current_user)
     job_title = models.CharField(max_length=220)
     job_context = models.TextField()
     company_name = models.CharField(max_length=120)
