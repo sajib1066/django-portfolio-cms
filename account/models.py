253a2109f84aa527bee665909c0a772f7e276411
sajib1066 <sajib1066@gmail.com>
2020-06-07 11:32:30 +0600

    update model

--- account/models.py
+++ account/models.py
@@ -121,5 +121,8 @@ class SelectedTheme(models.Model):
     theme = models.ForeignKey(Theme, on_delete=models.CASCADE, blank=True, null=True)
     user = models.ForeignKey('account.User', on_delete=models.CASCADE)
 
+    class Meta:
+        unique_together = ['theme', 'user']
+
     def __str__(self):
         return self.theme.name
