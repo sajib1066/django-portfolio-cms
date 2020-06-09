cc9160cccc3ce8e070e372ff6925f6aade2dfd20
sajib1066 <sajib1066@gmail.com>
2020-06-09 15:45:54 +0600

    delete experience app

--- experience/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from .models import Experience
-
-admin.site.register(Experience)
--- experience/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import add_experience, experience_list
-
-urlpatterns = [
-    path('add/', add_experience, name='add-experience'),
-    path('list/', experience_list, name='experience-list'),
-]
-
