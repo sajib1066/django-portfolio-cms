5937ba6455ce1d2f27aeae87cbc5dd45b5b71d7c
sajib1066 <sajib1066@gmail.com>
2020-06-10 19:55:18 +0600

    update theme model

--- theme/models.py
+++ theme/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Theme(models.Model):
     name = models.CharField(max_length=30, unique=True)
     preview = models.ImageField(upload_to='theme-preview/')
+    file_name = models.CharField(max_length=30, unique=True)
     theme_url = models.CharField(max_length=30, unique=True)
 
     def __str__(self):
