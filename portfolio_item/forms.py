4d5e101c78fac5928453a23030f339f0ccc43cda
sajib1066 <sajib1066@gmail.com>
2020-06-11 20:11:49 +0600

    create date field

--- portfolio_item/forms.py
+++ portfolio_item/forms.py
@@ -38,8 +38,8 @@ class ExperienceForm(forms.ModelForm):
             'job_title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Job Title'}),
             'job_context': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Job Context'}),
             'company_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Company Name'}),
-            'start_date': forms.TextInput(attrs={'class': 'form-control'}),
-            'end_date': forms.TextInput(attrs={'class': 'form-control'}),
+            'start_date': forms.TextInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'end_date': forms.TextInput(attrs={'class': 'form-control', 'type': 'date'}),
         }
 
 
